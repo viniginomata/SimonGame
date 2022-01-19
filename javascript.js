002e5007426f7705c7eb73f7ca02fada1c30a631
Vinicius Inomata <89132503+viniginomata@users.noreply.github.com>
2022-01-19 20:10:48 +0900

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,41 @@
+
+let buttonColours = ["red","blue","green","yellow"];
+
+let gamePattern = []
+
+let userClickedPattern = []
+
+$(".btn").click(function(){      /* on click runs the function */
+    let userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    console.log(userChosenColour);
+    playSound(userChosenColour)
+    animatePress(userChosenColour);
+    
+})
+
+    function nextSequence(){  /* generates a new sequence */
+        let randomNumber = Math.floor(Math.random()*4);
+        let randomChosenColour = buttonColours[randomNumber];  
+        gamePattern.push(randomChosenColour);
+        $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+        playSound(randomChosenColour)
+    
+
+};
+
+    function playSound(name){
+        var audio = new Audio("sounds/"+ name + ".mp3");
+        audio.play();
+}
+    
+    function animatePress(currentColour){
+        $("."+currentColour).addClass("pressed")
+        setTimeout(function(){
+            $("."+currentColour).removeClass("pressed")
+        },100);
+    }
+
+    
+
+
